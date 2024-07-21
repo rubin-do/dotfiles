ebb9f77a3a11c8eb98094ff6700a16bd32d9b686
Daniil Rubin <danyarubin@gmail.com>
2024-07-21 21:14:11 +0300

    Enable nf, add stylua

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -12,7 +12,7 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
