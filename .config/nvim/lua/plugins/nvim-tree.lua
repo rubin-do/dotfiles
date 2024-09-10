018e1df6dac943446e2b5230edb4cd52483fd612
Daniil Rubin <danyarubin@gmail.com>
2024-09-10 21:01:25 +0300

    Finish refactoring config

---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,7 @@
+-- TODO: plugin needed?
+return {
+  {
+    'https://github.com/kyazdani42/nvim-tree.lua',
+    opts = {},
+  },
+}
---
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,10 @@
+-- Highlight todo, notes, etc in comments
+-- TODO: highlight todos in format TODO(daniilrubin)
+return {
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+}
